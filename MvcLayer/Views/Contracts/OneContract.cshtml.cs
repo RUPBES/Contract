4b7b40a1f1500a6a92e30328c450058ffbaae98d
zhadejko.aa@rupbes.by <zhadejko.aa@rupbes.by>
2023-07-24 16:07:44 +0300

    change main table

---
+++ MvcLayer/Views/Contracts/OneContract.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MvcLayer.Views.Contracts
+{
+    public class OneContractModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
