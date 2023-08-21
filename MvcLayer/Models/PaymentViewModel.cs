d46fb99caaf68673d696596393f0eb3749085fa1
zhadejko.aa@rupbes.by <zhadejko.aa@rupbes.by>
2023-08-21 16:49:10 +0300

    add payment controller

---
+++ MvcLayer/Models/PaymentViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace MvcLayer.Models
+{
+    public class PaymentViewModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Текущие авансы")]
+        public decimal? PaySum { get; set; }
+
+        [DisplayName("Текущие авансы по факту")]
+        public decimal? PaySumForRupBes { get; set; }        
+
+        [DisplayName("Месяц за который получено")]
+        public DateTime? Period { get; set; }
+
+        [DisplayName("Изменено?")]
+        public bool? IsChange { get; set; }
+
+        [DisplayName("ID Контракт")]
+        public int? ContractId { get; set; }
+
+        public virtual ContractViewModel Contract { get; set; }
+    }
+}
