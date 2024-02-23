952ac95048f14ddf6d2492a4be7c9eaf7d68a6b9
zhadejko.aa@rupbes.by <zhadejko.aa@rupbes.by>
2024-02-23 12:31:46 +0300

    change view scope create, add filter on price

---
+++ MvcLayer/Models/Reports/ScopeWorkContractViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace MvcLayer.Models.Reports
+{
+    public class ScopeWorkContractViewModel
+    {
+        ItemScopeWorkContract? contractPrice;
+        ItemScopeWorkContract? contractPriceOwn;
+        ItemScopeWorkContract? workTodayYear;
+        ItemScopeWorkContract? workTodayYearOwn;
+        ItemScopeWorkContract? remainingScope;
+        ItemScopeWorkContract? remainingScopeOwn;
+        ItemScopeWorkContract? todayScope;
+        ItemScopeWorkContract? todayScopeOwn;
+        List<ItemScopeWorkContract> scopes;
+    }
+
+    public class ItemScopeWorkContract : ScopeWorkForReport
+    {
+        public decimal? TotalCost { get; set; }
+        public decimal? TotalWithoutNds { get; set; }
+    }
+}
